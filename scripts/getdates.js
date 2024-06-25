0c9ef6188a4a05a884d26993e317f2616017e777
KierstiR <kiersti.roberts@gmail.com>
2024-06-25 15:04:57 -0600

    created personal website homepage assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,2 @@
+document.getElementById('currentyear').textContent = new Date().getFullYear(); /* Sets the current year in the footer */
+document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`; /* Sets the last modified date in the footer */
